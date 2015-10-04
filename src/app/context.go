xjdrew/redis-persist

add check command to compare data on disk with redis.

src/app/context.go
@@ -80,6 +80,37 @@ func sync_all(ud interface{}, args []string) (result string, err error) {
 	return
 }
 
+func check(ud interface{}, args []string) (result string, err error) {
+	context := ud.(*Context)
+	db := context.db
+	cli := context.redis
+	it := db.NewIterator()
+	defer it.Close()
+	var leveldb_data []string
+	count := 0
+	mismatch_count := 0
+	for it.SeekToFirst(); it.Valid(); it.Next() {
+		if json_err := json.Unmarshal(it.Value(), &leveldb_data); json_err != nil {
+			log.Printf("json unmarshal err:%v", json_err)
+			log.Printf("it.Value():%v", it.Value())
+		}
+		redis_data, err_redis := cli.Hgetall_arr(string(it.Key()))
+		if err_redis != nil {
+			log.Printf("redis err:%v", err_redis)
+		}
+		for i, redis_section := range redis_data {
+			if redis_section != leveldb_data[i] {
+				log.Printf("key mismatch:%v", it.Key())
+				mismatch_count++
+				break
+			}
+		}
+		count++
+	}
+	result = fmt.Sprintf("%d counts, %d keys mismatch\n", count, mismatch_count)
+	return
+}
+
 func dump(ud interface{}, args []string) (result string, err error) {
 	if len(args) == 0 {
 		err = errors.New("no key")
@@ -230,6 +261,7 @@ func (context *Context) Register(c *CmdService) {
 	c.Register("diff", context, diff)
 	c.Register("shutdown", context, shutdown)
 	c.Register("keys", context, keys)
+	c.Register("check", context, check)
 }
 
 func NewContext() *Context {

spin6lock/redis-land-go

src/app/context.go
@@ -106,6 +106,48 @@ func count(ud interface{}, args []string) (result string, err error) {
 	return
 }
 
+func check(ud interface{}, args []string) (result string, err error) {
+	context := ud.(*Context)
+	db := context.db
+	cli := context.redis
+	it := db.NewIterator()
+	var leveldb_data map[string]string
+	count := 0
+	mismatch_count := 0
+	all_key_strings, err := cli.Exec("keys", "*")
+	redis_key_count := len(all_key_strings.([]string))
+	for it.SeekToFirst(); it.Valid(); it.Next() {
+		if json_err := json.Unmarshal(it.Value(), &leveldb_data); json_err != nil {
+			log.Printf("json unmarshal err:%v", json_err)
+			log.Printf("it.Value():%v", it.Value())
+		}
+		redis_data := make(map[string]string)
+		err_redis := cli.Hgetall(string(it.Key()), redis_data)
+		if err_redis != nil {
+			log.Printf("redis err:%v", err_redis)
+		}
+		for key, value := range redis_data {
+			if value != leveldb_data[key] {
+				log.Printf("key mismatch:%v", string(it.Key()))
+				mismatch_count++
+				break
+			}
+		}
+		count++
+	}
+	result = fmt.Sprintf("%d counts, %d keys mismatch\n", count, mismatch_count)
+	switch {
+	case count > redis_key_count:
+		result = result + fmt.Sprintf("redis key amount is less than leveldb:%d vs %d", redis_key_count, count)
+	case count < redis_key_count:
+		result = result + fmt.Sprintf("redis key amount is larger than leveldb:%d vs %d", redis_key_count, count)
+	default:
+		result = result + fmt.Sprintf("%d key compared, %d mismatch", count, mismatch_count)
+	}
+
+	return
+}
+
 func fast_check(ud interface{}, args []string) (result string, err error) {
 	begin_timestamp := time.Now()
 	context := ud.(*Context)
@@ -149,7 +191,6 @@ func fast_check(ud interface{}, args []string) (result string, err error) {
 		result = result + fmt.Sprintf("%d key compared, %d mismatch", count, mismatch_count)
 	}
 	return
-
 }
 
 func dump(ud interface{}, args []string) (result string, err error) {
@@ -332,6 +373,7 @@ func (context *Context) Register(c *CmdService) {
 	c.Register("diff", context, diff)
 	c.Register("shutdown", context, shutdown)
 	c.Register("keys", context, keys)
+	c.Register("check", context, check)
 	c.Register("fast_check", context, fast_check)
 	c.Register("restore_one", context, restore_one)
 }